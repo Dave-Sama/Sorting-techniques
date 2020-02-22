4649d84d48bd49f7de389d4100be6bee89a2ae80
Dave-Sama <55929174+Dave-Sama@users.noreply.github.com>
2020-02-22 15:24:27 +0200

    updated my radix sort.
    added some upgrades/

--- radix_sort.py
+++ radix_sort.py
@@ -49,42 +49,31 @@ def radix(a):
     while maxi > 0:
         count += 1
         maxi = maxi // 10
-    j = 0
 
     b = [None for _ in range(len(a)+1)]
     c = []
     print(b)
-    for i in a:
-        if b[i%10] == None:
-            b[i % 10] = Linked(new_node(i))
-        else:
-            b[i % 10].append(new_node(i))
 
-    for i in b:
-        if i != None:
-            c = c + i.traversal()
-    j = 1
-    b = [None for _ in range(len(a)+1)]
-    num = 10
+    j = 0
+    num = 1
     while j < count:
 
-        for i in c:
-                digit = (i//num)%10
-
-                if b[digit] == None:
-                    b[digit] = Linked(new_node(i))
+        for i in a:
+                if b[(i//num)%10] == None:
+                    b[(i//num)%10] = Linked(new_node(i))
                 else:
-                    b[digit].append(new_node(i))
+                    b[(i//num)%10].append(new_node(i))
 
         c = []
         for i in b:
             if i != None:
                 c = c + i.traversal()
+        a = c
         b = [None for _ in range(len(a) + 1)]
 
         j += 1
-        num = pow(num, 2)
-    return c
+        num = int(str(num) + '0')
+    return a
 
 
 a = [4, 98, 110, 74, 9, 31, 199, 36, 0]
